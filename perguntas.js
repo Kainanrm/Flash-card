0b33736de31c933173af37790a34b054fe9a5761
Kainanrm <161533850+Kainanrm@users.noreply.github.com>
2024-12-05 15:09:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,93 @@
+criaCartao(
+    'Português',
+    'O que é verbo',
+    'verbo é  é a palavra que expressa ações, estados e fenômenos da natureza.'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz oi em inglês?',
+    'HI'
+)
+
+criaCartao(
+    'História',
+    'Quem comandou a guerra dos canudos?',
+    'Atonio conselheiro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da BAHIA?',
+    'SALVADOR'
+)
+
+criaCartao(
+    'Filosofia',
+    'Cite o nome de um filosofo importante',
+    'Sócrates'
+)
+    criaCartao(
+        'Sociologia',
+        'Cite os três poderes',
+        'Judiciario, Executivo e Legislativo'
+
+    )
+
+    criaCartao(
+        'Matemática',
+        'Quanto e 30x40?',
+        '1200'
+    )
+
+    criaCartao(
+        'Física',
+        'Como chama o nascimento da estrela?',
+        'Nebulosa'
+    )
+
+    criaCartao(
+        'Química',
+        'Quantas ligacao o carbono faz?',
+        ' 4 ligacoes'
+    )
+
+    criaCartao(
+        'Biologia',
+        'Qual a função do coração?',
+        'Bombear Sangue'
+    )
+
+    criaCartao(
+        'Artes',
+        'Quais as cores primarias?',
+        'Vermelho, Azul, Amarelo'
+    )
+
+    criaCartao(
+        'Educação Física',
+        'Quais as capacidades físicas',
+        'Resistência, Força, Flexibilidade, Agilidade e Velocidade.'
+    )
+
+    criaCartao(
+        'Tecnologia',
+        'O que backgroud',
+        'Cor de fundo'
+    )
+    criaCartao(
+        'Educação Financeira',
+        'Quais os tipos de investidores',
+        'Conservador, Moderado e Arrojado'
+    )
+
+    criaCartao(
+        'Empreendedorismo',
+        'O que e empreender?',
+        'Criar uma empresa'
+    )
+    criaCartao(
+        'Biotecnologia',
+        'O que e meiose',
+        'A meiose é uma divisão celular que leva à formação de quatro células-filhas'
+    )
